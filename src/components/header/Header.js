6d8c3df57968964f1e37bf9c0546c9f12c1a5349
Nabilbentamer <76843256+Nabilbentamer@users.noreply.github.com>
2021-06-13 21:08:02 +0100

    update portfolio name

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -41,12 +41,12 @@ function Header() {
         <ul className={isDark ? "dark-menu menu" : "menu"}>
           {viewSkills && (
             <li>
-              <a href="#skills">Skills</a>
+              <a href="#skills">Compétences</a>
             </li>
           )}
           {viewExperience && (
             <li>
-              <a href="#experience">Work Experiences</a>
+              <a href="#experience">Experiences</a>
             </li>
           )}
           {viewOpenSource && (
@@ -70,7 +70,7 @@ function Header() {
             </li>
           )}
           <li>
-            <a href="#contact">Contact Me</a>
+            <a href="#contact">Contactez Moi</a>
           </li>
           <li>
             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
