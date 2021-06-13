6e025a5b72218f7f9bea8145e6e5b0e4533fe12e
Nabilbentamer <76843256+Nabilbentamer@users.noreply.github.com>
2021-06-13 18:28:56 +0100

    update username

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,7 +11,7 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
+  username: "Nabil BenTamer",
   title: "Hi all, I'm Saad",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
